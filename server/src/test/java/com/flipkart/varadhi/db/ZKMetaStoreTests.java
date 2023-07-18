3dfb88b7f76464151e9820180d39a3de2dcb78e1
kunalsharma.k <kunalsharma.k@flipkart.com>
2023-07-18 13:24:28 +0530

    added delete and list test cases

--- server/src/test/java/com/flipkart/varadhi/db/ZKMetaStoreTests.java
+++ server/src/test/java/com/flipkart/varadhi/db/ZKMetaStoreTests.java
@@ -1,5 +1,6 @@
 package com.flipkart.varadhi.db;
 
+import com.flipkart.varadhi.exceptions.MetaStoreException;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
@@ -10,11 +11,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 public class ZKMetaStoreTests {
 
     private static TestingServer zkCuratorTestingServer;
-    private static CuratorFramework zkCuratorFramework;
 
     private static ZKMetaStore zkMetaStore;
 
@@ -22,9 +23,8 @@ public class ZKMetaStoreTests {
     @BeforeAll
     public static void setUp() throws Exception {
         zkCuratorTestingServer = new TestingServer();
-        zkCuratorFramework =
-                CuratorFrameworkFactory.newClient(
-                        zkCuratorTestingServer.getConnectString(), new ExponentialBackoffRetry(0, 1));
+        CuratorFramework zkCuratorFramework = CuratorFrameworkFactory.newClient(
+                zkCuratorTestingServer.getConnectString(), new ExponentialBackoffRetry(1000, 1));
         zkCuratorFramework.start();
         zkMetaStore = new ZKMetaStore(zkCuratorFramework);
     }
@@ -34,12 +34,29 @@ public class ZKMetaStoreTests {
         zkCuratorTestingServer.close();
     }
 
-
     @Test
-    public void testGetZKData() {
-        zkMetaStore.create("sample-testing-data", 1, "/sample-testing-node");
+    public void testZKData() {
+
+        String response = zkMetaStore.create("sample-testing-data", 1, "/sample-testing-node");
+        Assertions.assertEquals(response, "/sample-testing-node");
+
         String data = zkMetaStore.get("/sample-testing-node", String.class);
         Assertions.assertEquals(data, "sample-testing-data");
+
+        zkMetaStore.create("", 1, "/sample-testing-node/1");
+        zkMetaStore.create("", 1, "/sample-testing-node/2");
+        List<String> zkChildrenNodes = zkMetaStore.list("/sample-testing-node");
+        Assertions.assertEquals(2, zkChildrenNodes.size());
+
+
+        zkMetaStore.delete("/sample-testing-node/1");
+        zkMetaStore.delete("/sample-testing-node/2");
+        zkMetaStore.delete("/sample-testing-node");
+        MetaStoreException exception = Assertions.assertThrows(
+                MetaStoreException.class,
+                () -> zkMetaStore.get("/sample-testing-node", String.class)
+        );
+        Assertions.assertEquals("Failed to get entity /sample-testing-node ", exception.getMessage());
     }
 
 }
