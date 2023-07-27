abc0a40b565611d66cfed4fc2ed03325545fccac
kunalsharma.k <kunalsharma.k@flipkart.com>
2023-07-27 15:25:27 +0530

    remove unused imports

--- server/src/main/java/com/flipkart/varadhi/web/handlers/TopicSchemaValidationHandler.java
+++ server/src/main/java/com/flipkart/varadhi/web/handlers/TopicSchemaValidationHandler.java
@@ -29,12 +29,13 @@ public class TopicSchemaValidationHandler implements ValidationHandler {
                 Validator.create(
                                 JsonSchema.of(schema),
                                 new JsonSchemaOptions().setDraft(Draft.DRAFT7).setBaseUri("http://127.0.0.1")
-                                        .setOutputFormat(OutputFormat.Basic)
+                                        .setOutputFormat(OutputFormat.Flag)
                         )
                         .validate(event.body().asJsonObject());
         if (outputUnit.getValid()) {
             event.next();
         }
+        response.putHeader("Content-Type", "application/json");
         response.setStatusCode(400);
         JsonObject jsonObject =
                 new JsonObject().put("error", "TopicSchemaValidation Failure");
