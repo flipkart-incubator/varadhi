17a0a9d3e5a1200c44df459455a523de90acf9c4
kunalsharma.k <kunalsharma.k@flipkart.com>
2023-07-27 13:49:51 +0530

    added TopicSchemaValidationhandler

--- server/src/main/java/com/flipkart/varadhi/web/handlers/TopicSchemaValidationHandler.java
+++ server/src/main/java/com/flipkart/varadhi/web/handlers/TopicSchemaValidationHandler.java
@@ -1,12 +1,10 @@
 package com.flipkart.varadhi.web.handlers;
 
+import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.validation.ValidationHandler;
-import io.vertx.json.schema.JsonSchema;
-import io.vertx.json.schema.JsonSchemaOptions;
-import io.vertx.json.schema.OutputUnit;
-import io.vertx.json.schema.Validator;
+import io.vertx.json.schema.*;
 import lombok.extern.slf4j.Slf4j;
 
 import static io.vertx.json.schema.common.dsl.Keywords.maxLength;
@@ -19,19 +17,27 @@ public class TopicSchemaValidationHandler implements ValidationHandler {
     @Override
     public void handle(RoutingContext event) {
 
-        event.request().bodyHandler(bodyHandler -> {
-            final JsonObject body = bodyHandler.toJsonObject();
-            JsonObject schema = objectSchema()
-                    .requiredProperty("name", stringSchema().with(minLength(5)).with(maxLength(100)))
-                    .requiredProperty("project", stringSchema().with(minLength(5)).with(maxLength(100)))
-                    .requiredProperty("grouped", booleanSchema())
-                    .toJson();
+        HttpServerResponse response = event.response();
 
-            OutputUnit outputUnit = Validator.create(JsonSchema.of(schema), new JsonSchemaOptions()).validate(body);
-            if (outputUnit.getValid()) {
-                event.next();
-            }
-            event.fail(400);
-        });
+        JsonObject schema = objectSchema()
+                .requiredProperty("name", stringSchema().with(minLength(5)).with(maxLength(100)))
+                .requiredProperty("project", stringSchema().with(minLength(5)).with(maxLength(100)))
+                .requiredProperty("grouped", booleanSchema())
+                .toJson();
+
+        OutputUnit outputUnit =
+                Validator.create(
+                                JsonSchema.of(schema),
+                                new JsonSchemaOptions().setDraft(Draft.DRAFT7).setBaseUri("http://127.0.0.1")
+                                        .setOutputFormat(OutputFormat.Basic)
+                        )
+                        .validate(event.body().asJsonObject());
+        if (outputUnit.getValid()) {
+            event.next();
+        }
+        response.setStatusCode(400);
+        JsonObject jsonObject =
+                new JsonObject().put("error", "TopicSchemaValidation Failure");
+        response.end(jsonObject.encodePrettily());
     }
 }
