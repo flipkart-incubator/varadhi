43ee29fa8e123ed3aeedcdd69bb6fca5706b4354
aayustark007-fk <aayush.gupta@flipkart.com>
2023-09-23 23:26:08 +0530

    address some PR comments

--- server/src/main/java/com/flipkart/varadhi/auth/DefaultAuthorizationProvider.java
+++ server/src/main/java/com/flipkart/varadhi/auth/DefaultAuthorizationProvider.java
@@ -1,17 +1,16 @@
 package com.flipkart.varadhi.auth;
 
 import com.flipkart.varadhi.entities.UserContext;
-import com.flipkart.varadhi.exceptions.InvalidConfigException;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.flipkart.varadhi.auth.ResourceAction.ORG_CREATE;
-
+@Slf4j
 public class DefaultAuthorizationProvider implements AuthorizationProvider {
     private DefaultAuthorizationConfiguration configuration;
 
@@ -24,26 +23,37 @@ public class DefaultAuthorizationProvider implements AuthorizationProvider {
     /**
      * Checks if the user {@code UserContext} is authorized to perform action {@code ResourceAction} on resource path.
      *
-     * @param userContext information on the user, contains the user identifier
-     * @param action      the action being performed by the user which is to be authorized
-     * @param resource    the path of the resource on which the action is to be authorized
+     * @param userContext Information on the user, contains the user identifier
+     * @param action      Action being performed by the user which is to be authorized
+     * @param resource    URI of the resource on which the action is to be authorized.
+     *                    Must be of format: {org_id} or {org_id}/{team_id} or {project_id}
+     *                    or {project_id}/{topic|queue|subscription}
      *
      * @return {@code Future<Boolean>} a future result expressing True/False decision
      */
     @Override
     public Future<Boolean> isAuthorized(UserContext userContext, ResourceAction action, String resource) {
         // parse the resource path based on the action and return the final resourceIDs for each resourceType
-        Map<ResourceType, String> resourceTypeToResourceId = getResourceIds(action, resource);
+        Map<ResourceType, String> resourceTypeToResourceId = getResourceIdsFromResourceURI(action, resource);
         return Future.succeededFuture(resourceTypeToResourceId.entrySet().stream()
                 .anyMatch(entry -> isAuthorizedInternal(userContext.getSubject(), action, entry.getValue())));
     }
 
-    private Map<ResourceType, String> getResourceIds(ResourceAction action, String resource) {
+    /**
+     * Takes the {@code resource} as a URI and breaks it down into individual pairs of {@code ResourceType} and their resourceIds
+     *
+     * @param action   ResourceAction to validate against (mostly required for resolving leaf nodes in the hierarchy)
+     * @param resource Full path to the resource on which the action is being performed (URI without any scheme)
+     *
+     * @return Mapping of each ResourceType to its respective resourceID in context of the current resource path
+     */
+    private Map<ResourceType, String> getResourceIdsFromResourceURI(ResourceAction action, String resource) {
         // TODO(aayush): need better way to do this?
         Map<ResourceType, String> resultMap = new HashMap<>();
         String[] segments = resource.split("/");
         return resolve(ResourceType.ORG, action, List.of(segments), resultMap);
     }
+
 
     private Map<ResourceType, String> resolve(
             ResourceType current, ResourceAction action, List<String> segments, Map<ResourceType, String> result
@@ -82,11 +92,20 @@ public class DefaultAuthorizationProvider implements AuthorizationProvider {
     }
 
     private boolean isAuthorizedInternal(String subject, ResourceAction action, String resourceId) {
+        log.debug(
+                "Checking authorization for subject [{}] and action [{}] on resource [{}]", subject, action,
+                resourceId
+        );
         return getRolesForSubject(subject, resourceId).stream()
-                .anyMatch(role -> doesActionBelongToRole(role, action));
+                .anyMatch(role -> doesActionBelongToRole(subject, role, action));
     }
 
-    private boolean doesActionBelongToRole(String roleId, ResourceAction action) {
-        return configuration.getRoles().getOrDefault(roleId, new ArrayList<>()).contains(action);
+    private boolean doesActionBelongToRole(String subject, String roleId, ResourceAction action) {
+        log.debug("Evaluating action [{}] for subject [{}] against role [{}]", action, subject, roleId);
+        boolean matching = configuration.getRoles().getOrDefault(roleId, new ArrayList<>()).contains(action);
+        if (matching) {
+            log.debug("Successfully matched action [{}] for subject [{}] against role [{}]", action, subject, roleId);
+        }
+        return matching;
     }
 }
