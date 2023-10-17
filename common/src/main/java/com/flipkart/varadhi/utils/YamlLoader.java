7c707bacc7c86c4876c2823f81d045f557fe94e2
aayustark007-fk <aayush.gupta@flipkart.com>
2023-10-17 14:32:02 +0530

    replace snakeyaml with jackson yaml factory object mapper

--- common/src/main/java/com/flipkart/varadhi/utils/YamlLoader.java
+++ common/src/main/java/com/flipkart/varadhi/utils/YamlLoader.java
@@ -1,16 +1,22 @@
 package com.flipkart.varadhi.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.flipkart.varadhi.exceptions.VaradhiException;
-import org.yaml.snakeyaml.Yaml;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.File;
 
 public class YamlLoader {
 
+    private static final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+
+    static {
+        mapper.findAndRegisterModules();
+    }
+
     public static <T> T loadConfig(String configFile, Class<T> clazz) {
         try {
-            return new Yaml().loadAs(Files.readString(Path.of(configFile)), clazz);
+            return mapper.readValue(new File(configFile), clazz);
         } catch (Exception e) {
             throw new VaradhiException(String.format("Failed to load config file: %s as %s.", configFile, clazz), e);
         }
