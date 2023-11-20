29063fa70fcb1451e600561d78cd3b9562a16a8b
aayustark007-fk <aayush.gupta@flipkart.com>
2023-11-20 12:20:20 +0530

    add calls to authz server for default provider

--- authz/src/main/java/com/flipkart/varadhi/authz/DefaultAuthorizationProvider.java
+++ authz/src/main/java/com/flipkart/varadhi/authz/DefaultAuthorizationProvider.java
@@ -1,13 +1,14 @@
 package com.flipkart.varadhi.authz;
 
-import com.flipkart.varadhi.auth.DefaultAuthorizationConfiguration;
-import com.flipkart.varadhi.auth.ResourceAction;
-import com.flipkart.varadhi.auth.ResourceType;
+import com.flipkart.varadhi.auth.*;
 import com.flipkart.varadhi.entities.UserContext;
 import com.flipkart.varadhi.exceptions.InvalidConfigException;
 import com.flipkart.varadhi.utils.YamlLoader;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.ext.web.client.CachingWebClient;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.codec.BodyCodec;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
@@ -21,6 +22,7 @@ import java.util.Set;
 @Slf4j
 public class DefaultAuthorizationProvider implements AuthorizationProvider {
     private DefaultAuthorizationConfiguration configuration;
+    private WebClient webClient;
     private volatile boolean initialised = false;
 
     @Override
@@ -29,6 +31,7 @@ public class DefaultAuthorizationProvider implements AuthorizationProvider {
             this.configuration =
                     YamlLoader.loadConfig(
                             authorizationOptions.getConfigFile(), DefaultAuthorizationConfiguration.class);
+            this.webClient = CachingWebClient.create(WebClient.create(vertx));
             this.initialised = true;
         }
         return Future.succeededFuture(true);
@@ -51,7 +54,7 @@ public class DefaultAuthorizationProvider implements AuthorizationProvider {
         }
         List<Pair<ResourceType, String>> leafToRootResourceIds = resolveOrderedFromLeaf(action, resource);
         var result = leafToRootResourceIds.stream().filter(pair -> StringUtils.isNotBlank(pair.getValue()))
-                .anyMatch(pair -> isAuthorizedInternal(userContext.getSubject(), action, pair.getValue()));
+                .anyMatch(pair -> isAuthorizedInternalV2(userContext.getSubject(), action, pair.getValue()).result());
         return Future.succeededFuture(result);
     }
 
@@ -93,10 +96,34 @@ public class DefaultAuthorizationProvider implements AuthorizationProvider {
                 .anyMatch(role -> doesActionBelongToRole(subject, role, action));
     }
 
+    private Future<Boolean> isAuthorizedInternalV2(String subject, ResourceAction action, String resourceId) {
+        log.debug(
+                "Checking authorization for subject [{}] and action [{}] on resource [{}]", subject, action,
+                resourceId
+        );
+        return getRolesForSubjectV2(subject, resourceId)
+                .compose(roles -> checkAllRoles(subject, roles, action));
+    }
+
     private Set<String> getRolesForSubject(String subject, String resourceId) {
         return configuration.getRoleBindings()
                 .getOrDefault(resourceId, Map.of())
                 .getOrDefault(subject, Set.of());
+    }
+
+
+    private Future<Set<String>> getRolesForSubjectV2(String subject, String resourceId) {
+        // use web client to make get call
+        return webClient.get(8088, "localhost", "/v1/authz/rbs/" + resourceId)
+                .as(BodyCodec.json(RoleBindingNode.class))
+                .send()
+                .compose(response -> {
+                    RoleBindingNode node = response.body();
+                    return Future.succeededFuture(node.getSubjectToRolesMapping().getOrDefault(subject, Set.of()));
+                })
+                .onFailure(err -> log.error("Error while fetching roles for subject [{}] and resource [{}]",
+                        subject, resourceId, err
+                ));
     }
 
     private boolean doesActionBelongToRole(String subject, String roleId, ResourceAction action) {
@@ -106,6 +133,30 @@ public class DefaultAuthorizationProvider implements AuthorizationProvider {
             log.debug("Successfully matched action [{}] for subject [{}] against role [{}]", action, subject, roleId);
         }
         return matching;
+    }
+
+    private Future<Boolean> doesActionBelongToRoleV2(String subject, String roleId, ResourceAction action) {
+        log.debug("Evaluating action [{}] for subject [{}] against role [{}]", action, subject, roleId);
+        return webClient.get(8088, "localhost", "/v1/authz/roles/" + roleId)
+                .as(BodyCodec.json(Role.class))
+                .send()
+                .compose(response -> {
+                    Role role = response.body();
+                    boolean matching = role.getPermissions().contains(action);
+                    if (matching) {
+                        log.debug(
+                                "Successfully matched action [{}] for subject [{}] against role [{}]", action, subject,
+                                roleId
+                        );
+                    }
+                    return Future.succeededFuture(matching);
+                })
+                .onFailure(err -> log.error("Error while fetching details for role [{}]", roleId, err));
+    }
+
+    private Future<Boolean> checkAllRoles(String subject, Set<String> roles, ResourceAction action) {
+        return Future.all(roles.stream().map(role -> doesActionBelongToRoleV2(subject, role, action)).toList())
+                .map(result -> result.list().stream().anyMatch(Boolean.TRUE::equals));
     }
 
     private void pushOrgNode(List<Pair<ResourceType, String>> resourceIdTuples, String[] segments) {
@@ -130,8 +181,10 @@ public class DefaultAuthorizationProvider implements AuthorizationProvider {
             List<Pair<ResourceType, String>> resourceIdTuples, ResourceAction action, String[] segments
     ) {
         if (segments.length > 3) {
-            resourceIdTuples.add(Pair.of(action.getResourceType(),
-                    segments[2] + ":" + segments[3])); //{project_id}:{[topic|sub|queue]_id}
+            resourceIdTuples.add(Pair.of(
+                    action.getResourceType(),
+                    segments[2] + ":" + segments[3]
+            )); //{project_id}:{[topic|sub|queue]_id}
         }
     }
 }
