f0f08629df1bedd4ad3749194225c9cc9f16d7c8
Gaurav Ashok <gaurav.ashok@flipkart.com>
2023-12-18 18:18:22 +0530

    made the INITIAL_VERSION to 0. somehow it got changed to 1.

--- entities/src/main/java/com/flipkart/varadhi/entities/VaradhiResource.java
+++ entities/src/main/java/com/flipkart/varadhi/entities/VaradhiResource.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 @EqualsAndHashCode
 public class VaradhiResource implements BaseResource {
 
-    public static final int INITIAL_VERSION = 1;
+    public static final int INITIAL_VERSION = 0;
     public static final String NAME_SEPARATOR = ".";
 
     private final String name;
