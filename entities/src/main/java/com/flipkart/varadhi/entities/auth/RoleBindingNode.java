d39ebc42eecfb5cbe9b53d8bf02f644187878489
aayustark007-fk <aayush.gupta@flipkart.com>
2024-01-05 18:27:25 +0530

    make iam request validatable, role binding node is not

--- entities/src/main/java/com/flipkart/varadhi/entities/auth/RoleBindingNode.java
+++ entities/src/main/java/com/flipkart/varadhi/entities/auth/RoleBindingNode.java
@@ -2,7 +2,6 @@ package com.flipkart.varadhi.entities.auth;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.flipkart.varadhi.entities.ValidateResource;
 import com.flipkart.varadhi.entities.MetaStoreEntity;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -15,7 +14,6 @@ import java.util.Set;
 
 @Getter
 @EqualsAndHashCode(callSuper = true)
-@ValidateResource(message = "Invalid Role Binding name. Check naming constraints.")
 public class RoleBindingNode extends MetaStoreEntity {
     @NotBlank
     private final String resourceId;
