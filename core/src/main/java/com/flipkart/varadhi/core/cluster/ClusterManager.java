1bf5bc11c9f1241be6cb0c05c7c70a6ca19f1129
Gaurav Ashok <gaurav.ashok@flipkart.com>
2024-01-30 17:01:07 +0530

    Cluster Manager interface (#87)
    * removed bug related exception classes. we can just rely on standard java exceptions for these. Moved spi related interfaces to their own module. added module info in the README
    * Adding ClusterManager interface and interface to be able to connect to a cluster node.

---
+++ core/src/main/java/com/flipkart/varadhi/core/cluster/ClusterManager.java
@@ -0,0 +1,24 @@
+package com.flipkart.varadhi.core.cluster;
+
+import java.util.List;
+
+/**
+ * This interface is responsible for managing the cluster membership. Its implementation is responsible for capturing the
+ * node resources, local ip & hostname, so that they can be used while registering to the cluster and other nodes can
+ * be notified of the same.
+ */
+public interface ClusterManager {
+    void join();
+
+    void leave();
+
+    List<NodeInfo> getAllMembers();
+
+    void addMembershipListener(MembershipListener listener);
+
+    NodeConnection connect(String nodeId);
+
+    // TODO: Any publish to all methods?
+
+    // TODO: lock & leader election related methods to go in here.
+}
---
+++ core/src/main/java/com/flipkart/varadhi/core/cluster/NodeResources.java
@@ -0,0 +1,4 @@
+package com.flipkart.varadhi.core.cluster;
+
+public record NodeResources(int cpuCount, int networkBandwidthMBps) {
+}
