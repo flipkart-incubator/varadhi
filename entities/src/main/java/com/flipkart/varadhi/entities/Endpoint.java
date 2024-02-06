a937276f4551342f725c847895204c86bae0f819
aayustark007-fk <aayush.gupta@flipkart.com>
2024-02-06 14:11:01 +0530

    use NAME for json subtyping for Endpoint class

--- entities/src/main/java/com/flipkart/varadhi/entities/Endpoint.java
+++ entities/src/main/java/com/flipkart/varadhi/entities/Endpoint.java
@@ -8,7 +8,8 @@ import lombok.EqualsAndHashCode;
 import java.net.URL;
 
 @JsonTypeInfo(
-        use = JsonTypeInfo.Id.DEDUCTION
+        use = JsonTypeInfo.Id.NAME,
+        property = "@endpointType"
 )
 @JsonSubTypes({
         @JsonSubTypes.Type(Endpoint.HttpEndpoint.class),
