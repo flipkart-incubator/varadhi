f7745c9d52eb0701b59a9ea6c5492418ac30d5b9
kaur-prabhpreet <prabhpreet.kaur.bajwa@gmail.com>
2024-03-03 00:32:45 +0530

    configmap setup

--- scripts/benchmark_producer/deployment/gke/scripts/produce_to_topic.py
+++ scripts/benchmark_producer/deployment/gke/scripts/produce_to_topic.py
@@ -5,7 +5,6 @@ import string
 import os
 
 from util import handle_response, getPublishHeaders, getId
-from config import *
 
 class PayloadUsage:
     payload = ""
@@ -20,35 +19,14 @@ class MetaData:
 
 def new_payload(i):
     p = PayloadUsage()
+    MIN_MESSAGE_SIZE_KB = int(os.environ.get("MIN_MESSAGE_SIZE_KB", "2"))
+    MAX_MESSAGE_SIZE_KB = int(os.environ.get("MAX_MESSAGE_SIZE_KB", "8"))
+    RANDOM_PAYLOAD_MAX_USAGE = int(os.environ.get("RANDOM_PAYLOAD_MAX_USAGE", "10"))
     size = random.randint(MIN_MESSAGE_SIZE_KB * 1000, MAX_MESSAGE_SIZE_KB * 1000)
     p.payload = ''.join(random.choices(string.ascii_letters + string.digits, k=size))
     p.used = 0
     p.max_usage_allowed = (i % RANDOM_PAYLOAD_MAX_USAGE) + 1  # just to shuffle some usage.
     return p
-
-
-def get_topic_distribution():
-    buckets = [0] * TOPIC_RANDOMIZATION_BUCKETS
-    total_topics = TOPIC_END_IDX - TOPIC_START_IDX + 1
-
-
-    probabilties = list(TOPIC_PROBABILITIES) # clone
-    if len(probabilties) == 0:
-        # assuming uniform distribution
-        probabilties = list(map(lambda t: 100.0/total_topics, range(0, total_topics)))
-
-
-    cumulative_probability = 0.0
-    bucket_idx = 0
-    for i in range(0, total_topics):
-        cumulative_probability += probabilties[i]
-        bucket_end_idx = (cumulative_probability * TOPIC_RANDOMIZATION_BUCKETS / 100.0) - 1
-        while bucket_idx <= bucket_end_idx:
-            buckets[bucket_idx] = TOPIC_START_IDX + i
-            bucket_idx += 1
-    # take care of the last one
-    buckets[TOPIC_RANDOMIZATION_BUCKETS - 1] = TOPIC_END_IDX
-    return buckets
 
 
 def new_group_id():
@@ -61,7 +39,11 @@ def new_group_id():
 def publish_to_topic(task, topic_name, grouped=True, project_name="project-dec5"):
     groupId = task.user.get_group_id(topic_name, grouped)
     headers = getPublishHeaders(groupId)
+
+    PRODUCE_URL = os.environ.get("PRODUCE_URL")
     url = PRODUCE_URL.format(project_name, topic_name)
+
+    PRODUCE_ENDPOINT = os.environ.get("PRODUCE_ENDPOINT")
     data = '{{"content":"{}"}}'.format(task.user.get_request_payload())
     response = task.client.post(PRODUCE_ENDPOINT + url, headers=headers, data=data,
                                 timeout=(1, 60), verify=False, catch_response=True,
@@ -69,19 +51,9 @@ def publish_to_topic(task, topic_name, grouped=True, project_name="project-dec5"
     # Passing the catched response to handle_response utility method.
     handle_response(task, response, topic_name)
 
-
 def publish(task):
-    topic_id = task.user.get_random_topic_idx()
     topic_name = os.environ.get("TOPIC_NAME")
-
-
-    if topic_name is None:
-        print("TOPIC_NAME environment variable is not set.")
-    else:
-        print("Topic name:", topic_name)
-
-
-    project_name = "project-dec5"
+    project_name = os.environ.get("PROJECT_NAME")
     publish_to_topic(task, topic_name, project_name)
 
 
@@ -96,10 +68,10 @@ class PublishMessageTaskSet(TaskSet):
 class PublishMessageUser(HttpUser):
     host = ""
     metadataMap = {}
+    global RANDOM_PAYLOAD_POOL_SIZE
+    RANDOM_PAYLOAD_POOL_SIZE = int(os.environ.get("RANDOM_PAYLOAD_POOL_SIZE", "1024"))
     random_payloads = list(map(lambda i: new_payload(i + 1), range(0, RANDOM_PAYLOAD_POOL_SIZE)))
-    topic_distribution = get_topic_distribution()
     tasks = [PublishMessageTaskSet]
-
 
     def get_request_payload(self):
         idx = random.randint(0, RANDOM_PAYLOAD_POOL_SIZE - 1)
@@ -110,22 +82,14 @@ class PublishMessageUser(HttpUser):
         p.used += 1
         return p.payload
 
-
-    def get_random_topic_idx(self):
-        #random number to produce messages to topics in required ratio
-        rand_num = random.randint(0, TOPIC_RANDOMIZATION_BUCKETS - 1)
-        topic_id = self.topic_distribution[rand_num]
-        return topic_id
-
-
     def get_group_id(self, topic_name, grouped):
         if grouped :
             if topic_name in self.metadataMap:
                 # previous group is still ongoing. use the same group
+                MESSAGE_COUNT_PER_GROUP = int(os.environ.get("MESSAGE_COUNT_PER_GROUP", "20"))
                 if self.metadataMap[topic_name].messageCount < MESSAGE_COUNT_PER_GROUP:
                     self.metadataMap[topic_name].messageCount += 1
                     return self.metadataMap[topic_name].groupId
-
 
             # either the topic is not there or the group is complete
             new_group_metadata = new_group_id()
