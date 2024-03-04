4334aaaa60dc9d4b7af81e4dc7d82bdb0c40b5a7
gaurav-ashok <g.ashok.20@gmail.com>
2024-03-04 13:31:42 +0530

    ConsumerManager now takes shard name instead of raw shard id. shard id is more metadata centric concept, shard name is to identify the running shard. added project to startSub method as well.

--- consumer/src/main/java/com/flipkart/varadhi/consumer/ConsumersManager.java
+++ consumer/src/main/java/com/flipkart/varadhi/consumer/ConsumersManager.java
@@ -2,6 +2,7 @@ package com.flipkart.varadhi.consumer;
 
 import com.flipkart.varadhi.entities.ConsumptionPolicy;
 import com.flipkart.varadhi.entities.Endpoint;
+import com.flipkart.varadhi.entities.Project;
 import com.flipkart.varadhi.spi.services.TopicPartitions;
 import io.vertx.core.Future;
 
@@ -13,28 +14,28 @@ public interface ConsumersManager {
     /**
      * Start consumption for a shard of a subscription. If subscription failed, the future will contain the exception
      * details.
-     *
-     * @param subscription
+     * `shardName` identifies the different shards within the subscription.
      *
      * @return
      */
     Future<Void> startSubscription(
+            Project project,
             String subscription,
-            int shardId,
-            TopicPartitions<?> topics,
+            String shardName,
+            TopicPartitions<?> topic,
             boolean grouped,
             Endpoint endpoint,
             ConsumptionPolicy consumptionPolicy,
             ConsumptionFailurePolicy failurePolicy
     );
 
-    Future<Void> stopSubscription(String subscription, int shardId);
+    Future<Void> stopSubscription(String subscription, String shardName);
 
-    Future<Void> pauseSubscription(String subscription, int shardId);
+    Future<Void> pauseSubscription(String subscription, String shardName);
 
-    Future<Void> resumeSubscription(String subscription, int shardId);
+    Future<Void> resumeSubscription(String subscription, String shardName);
 
-    ConsumerState getConsumerState(String subscription, int shardId);
+    ConsumerState getConsumerState(String subscription, String shardName);
 
     // TODO likely need status on the starting / stopping as well; as the above status is for a running consumer..
 }
