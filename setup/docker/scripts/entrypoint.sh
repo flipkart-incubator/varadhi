0e4423055b1ad2df7f6ffb20e6915039a0f96a4f
iosdev747 <iosdev747.icloud@gmail.com>
2024-09-16 16:30:29 +0530

    add debug flags adding ports and java args

--- setup/docker/scripts/entrypoint.sh
+++ setup/docker/scripts/entrypoint.sh
@@ -15,8 +15,12 @@ JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote.port=9990"
 JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
 JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote.ssl=false"
 JAVA_OPTS="$JAVA_OPTS -Dlog4j2.configurationFile=/etc/varadhi/log4j2.xml"
-if "$APP_DEBUG"; then
-    JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
+
+# If debug flag is set(1 or true) then enable remote debugging
+if [[ ! -z "$APP_DEBUG" ]]; then
+    if [[ "$APP_DEBUG" == "1" || "$APP_DEBUG" == "true" ]]; then
+        JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005"
+    fi
 fi
 
 exec java -cp ./*:dependencies/* $JAVA_OPTS com.flipkart.varadhi.VaradhiApplication /etc/varadhi/configuration.yml
