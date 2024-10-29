b00d51516ef55575298eae4235618b60923d6a45
iosdev747 <iosdev747.icloud@gmail.com>
2024-10-29 12:56:36 +0530

    cleanup

--- server/src/main/java/com/flipkart/varadhi/verticles/controller/TrafficDataHandler.java
+++ server/src/main/java/com/flipkart/varadhi/verticles/controller/TrafficDataHandler.java
@@ -3,6 +3,7 @@ package com.flipkart.varadhi.verticles.controller;
 import com.flipkart.varadhi.cluster.messages.ClusterMessage;
 import com.flipkart.varadhi.cluster.messages.ResponseMessage;
 import com.flipkart.varadhi.controller.SuppressionManager;
+import com.flipkart.varadhi.controller.SuppressionService;
 import com.flipkart.varadhi.qos.entity.ClientLoadInfo;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,7 +11,7 @@ import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 public class TrafficDataHandler {
-    private final SuppressionManager suppressionManager;
+    private final SuppressionService suppressionManager;
 
     public TrafficDataHandler(SuppressionManager suppressionManager) {
         this.suppressionManager = suppressionManager;
