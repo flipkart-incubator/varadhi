df60a016c7913b1aa1ea1653db0a59e7e72a90ea
iosdev747 <iosdev747.icloud@gmail.com>
2024-11-06 16:43:21 +0530

    rename package ratelimiter

--- ratelimiter/src/main/java/module-info.java
+++ ratelimiter/src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module com.flipkart.varadhi.rateLimiter {
+module com.flipkart.varadhi.ratelimiter {
     requires static lombok;
     requires org.slf4j;
     exports com.flipkart.varadhi.qos.entity;
