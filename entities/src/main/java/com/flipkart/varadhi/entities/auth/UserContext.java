433b0d376aa0f9f98ad3c5968b3692c328ab2eca
Alekh Raj <alekh.raj@flipkart.com>
2025-01-28 09:43:42 +0530

    Removing TODO and adding the content to the interface description

--- entities/src/main/java/com/flipkart/varadhi/entities/auth/UserContext.java
+++ entities/src/main/java/com/flipkart/varadhi/entities/auth/UserContext.java
@@ -3,6 +3,11 @@ package com.flipkart.varadhi.entities.auth;
 import java.util.Map;
 import java.util.Optional;
 
+/*
+    * UserContext represents the authenticated user. It is used to pass the user information across the system.
+    * This can be enhanced to include more details like context around the authentication.
+*/
+
 public interface UserContext {
 
     String getSubject();
@@ -16,6 +21,4 @@ public interface UserContext {
     default Map<String, String> getAttributes() {
         return Map.of();
     }
-
-    // TODO: enhance it to include more details like context around the authentication
 }
