fa8f531a6c8674c7fb10ef3d9263a29bddea9a83
Alekh Raj <alekh.raj@flipkart.com>
2025-01-28 11:21:36 +0530

    rename method name from handle to authenticate

--- spi/src/main/java/com/flipkart/varadhi/spi/authn/AuthenticationProvider.java
+++ spi/src/main/java/com/flipkart/varadhi/spi/authn/AuthenticationProvider.java
@@ -1,12 +1,13 @@
 package com.flipkart.varadhi.spi.authn;
 
 import com.flipkart.varadhi.entities.auth.UserContext;
+import com.flipkart.varadhi.spi.ConfigFileResolver;
 import io.vertx.core.Future;
 
 import java.net.URI;
 import java.util.Map;
 
 public interface AuthenticationProvider {
-    Future<Boolean> init(AuthenticationOptions authenticationOptions);
-    Future<UserContext> handle(URI uri, Map<String,String> params, Map<String,String> headers, Map<String, Object> context);
+    Future<Boolean> init(ConfigFileResolver resolver, AuthenticationOptions authenticationOptions);
+    Future<UserContext> authenticate(URI uri, Map<String,String> params, Map<String,String> headers, Map<String, Object> context);
 }
