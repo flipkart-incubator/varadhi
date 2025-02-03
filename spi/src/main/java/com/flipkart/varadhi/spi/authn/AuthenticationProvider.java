2e3980f702ec8697650d6e5f1fd634ebd2f30b93
Alekh Raj <alekh.raj@flipkart.com>
2025-02-03 17:52:26 +0530

    reoving configresolver from the AuthenticationProvider interface

--- spi/src/main/java/com/flipkart/varadhi/spi/authn/AuthenticationProvider.java
+++ spi/src/main/java/com/flipkart/varadhi/spi/authn/AuthenticationProvider.java
@@ -1,13 +1,12 @@
 package com.flipkart.varadhi.spi.authn;
 
 import com.flipkart.varadhi.entities.auth.UserContext;
-import com.flipkart.varadhi.spi.ConfigFileResolver;
 import io.vertx.core.Future;
 
 import java.net.URI;
 import java.util.Map;
 
 public interface AuthenticationProvider {
-    Future<Boolean> init(ConfigFileResolver resolver, AuthenticationOptions authenticationOptions);
+    Future<Boolean> init(AuthenticationOptions authenticationOptions);
     Future<UserContext> authenticate(URI uri, Map<String,String> params, Map<String,String> headers, Map<String, Object> context);
 }
