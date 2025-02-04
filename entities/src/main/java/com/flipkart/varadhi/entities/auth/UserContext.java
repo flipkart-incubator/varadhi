36e447d9f01e806226ccd5994f2bcc13773f1e0a
Alekh Raj <alekh.raj@flipkart.com>
2025-02-04 11:12:22 +0530

    removing getOrg method from UserContext interface

--- entities/src/main/java/com/flipkart/varadhi/entities/auth/UserContext.java
+++ entities/src/main/java/com/flipkart/varadhi/entities/auth/UserContext.java
@@ -1,7 +1,6 @@
 package com.flipkart.varadhi.entities.auth;
 
 import java.util.Map;
-import java.util.Optional;
 
 /*
     * UserContext represents the authenticated user. It is used to pass the user information across the system.
@@ -13,10 +12,6 @@ public interface UserContext {
     String getSubject();
 
     boolean isExpired();
-
-    default Optional<String> getOrg() {
-        return Optional.empty();
-    }
 
     default Map<String, String> getAttributes() {
         return Map.of();
