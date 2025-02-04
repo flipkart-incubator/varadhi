61f0ce853b19193cd81f251155249cae38174556
alekhrj <alekhrj@gmail.com>
2025-02-04 16:46:28 +0530

    addding interface for AuthenticationHandlerProvider (#217)
    * addding interface for AuthenticationProvider
    * updating interface definition of spi. adding RequestContext class for authn spi

--- entities/src/main/java/com/flipkart/varadhi/entities/auth/UserContext.java
+++ entities/src/main/java/com/flipkart/varadhi/entities/auth/UserContext.java
@@ -1,10 +1,19 @@
 package com.flipkart.varadhi.entities.auth;
 
+import java.util.Map;
+
+/*
+    * UserContext represents the authenticated user. It is used to pass the user information across the system.
+    * This can be enhanced to include more details like context around the authentication.
+*/
+
 public interface UserContext {
 
     String getSubject();
 
     boolean isExpired();
 
-    // TODO: enhance it to include more details like context around the authentication
+    default Map<String, String> getAttributes() {
+        return Map.of();
+    }
 }
---
+++ entities/src/main/java/com/flipkart/varadhi/entities/utils/RequestContext.java
@@ -0,0 +1,14 @@
+package com.flipkart.varadhi.entities.utils;
+
+import lombok.Data;
+
+import java.net.URI;
+import java.util.Map;
+
+@Data
+public class RequestContext {
+    private URI uri;
+    private Map<String, String> params;
+    private Map<String, String> headers;
+    private Map<String, Object> context;
+}
