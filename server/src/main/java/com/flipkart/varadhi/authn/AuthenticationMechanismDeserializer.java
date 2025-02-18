b63b9a49752fcd07065d0453942e5f2a0d271c33
Alekh Raj <alekh.raj@flipkart.com>
2025-02-18 16:44:31 +0530

    code review changes - adding validation check for EnumDeserialiser - adding equal and hash code annotation to AuthenticationConfig

--- server/src/main/java/com/flipkart/varadhi/authn/AuthenticationMechanismDeserializer.java
+++ server/src/main/java/com/flipkart/varadhi/authn/AuthenticationMechanismDeserializer.java
@@ -11,7 +11,20 @@ public class AuthenticationMechanismDeserializer extends JsonDeserializer<Authen
     @Override
     public AuthenticationMechanism deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
         throws IOException {
-        String value = jsonParser.getText().toUpperCase();
-        return AuthenticationMechanism.valueOf(value);
+
+        validateJsonParser(jsonParser);
+        try {
+            String value = jsonParser.getText().toUpperCase();
+            return AuthenticationMechanism.valueOf(value);
+        } catch (IllegalArgumentException e) {
+            throw new IOException("Invalid authentication mechanism: " + jsonParser.getText(), e);
+        }
+    }
+
+    private void validateJsonParser(JsonParser jsonParser) throws IOException {
+        if (jsonParser == null || jsonParser.getText() == null) {
+            throw new IOException("JsonParser or authentication mechanism value cannot be null");
+        }
+
     }
 }
--- server/src/main/java/com/flipkart/varadhi/config/AuthenticationConfig.java
+++ server/src/main/java/com/flipkart/varadhi/config/AuthenticationConfig.java
@@ -5,8 +5,10 @@ import com.flipkart.varadhi.authn.AuthenticationMechanism;
 import com.flipkart.varadhi.authn.AuthenticationMechanismDeserializer;
 import com.flipkart.varadhi.spi.authn.AuthenticationOptions;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode (callSuper = true)
 public class AuthenticationConfig extends AuthenticationOptions {
 
     @JsonDeserialize (using = AuthenticationMechanismDeserializer.class)
