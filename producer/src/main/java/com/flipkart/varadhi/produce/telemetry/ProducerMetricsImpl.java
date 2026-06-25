4e771f3a41f3d4208ff152b022b052aab446df87
bandeep18 <bandeepkataria@gmail.com>
2026-06-26 04:59:00 +0530

    comments resolved

--- producer/src/main/java/com/flipkart/varadhi/produce/telemetry/ProducerMetricsImpl.java
+++ producer/src/main/java/com/flipkart/varadhi/produce/telemetry/ProducerMetricsImpl.java
@@ -52,7 +52,7 @@ public final class ProducerMetricsImpl implements ProducerMetrics {
         this.rejectedCount = Counter.builder("producer.rejected.count").tag("topic", topicFQN).register(registry);
 
         // relying on the registry to dedup it for other ProducerMetricsImpl instances. Since instance creation is rare
-        // operation, this is success.
+        // operation, this is ok.
         this.successTimer = Timer.builder("producer.latency")
                                  .tag("result", "success")
                                  .publishPercentileHistogram()
